c8ce7f4a5aca738607ccca449f957ad008ec6725
airajena <airajena.hack@gmail.com>
2025-09-02 12:38:23 +0530

    feat: Add simple HTTP server

---
+++ src/main/java/com/webserver/WebServerApp.java
@@ -0,0 +1,11 @@
+
+package com.webserver;
+
+public class WebServerApp {
+    public static void main(String[] args) {
+        System.out.println("🌟 Starting Simple Web Server...");
+
+        SimpleServer server = new SimpleServer(8080);
+        server.start();
+    }
+}
